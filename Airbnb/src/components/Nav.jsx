d5b0718031b0c092f1a945a9d981e5056447137e
Ricardo Ramos <ricardo.r.p466@outlook.com>
2022-10-11 11:31:17 -0400

    nav done

---
+++ Airbnb/src/components/Nav.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Nav.css'
+
+function Nav(){
+    return (
+        <div>
+            <nav className='nav'>
+                <img className='nav-logo' src="images/airbnb-logo.png" alt="Airbnb Logo" />
+            </nav>
+        </div>
+    )
+}
+export default Nav
